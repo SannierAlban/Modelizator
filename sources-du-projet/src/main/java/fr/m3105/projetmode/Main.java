7157eb8555fab7bf24a0fa8ea79e506edb55508a
François Deroubaix <francois.deoubaix.etu@univ-lille.fr>
2020-10-05 07:51:39 +0200

    fix project

--- sources-du-projet/src/main/java/fr/m3105/projetmode/Main.java
+++ sources-du-projet/src/main/java/fr/m3105/projetmode/Main.java
@@ -1,7 +1,5 @@
 package fr.m3105.projetmode;
 
-import fr.m3105.projetmode.utils.Model;
-import fr.m3105.projetmode.utils.ViewWindow;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,31 +9,14 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-<<<<<<< HEAD
-    public void start(Stage stage) {
-    	ViewWindow vw = new ViewWindow(800, 600, new Model());
-    	try {
-			vw.start(stage);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    }
-
-=======
     public void start(Stage PrimaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         PrimaryStage.setTitle("Main frame");
         PrimaryStage.setScene(new Scene(root));
         PrimaryStage.show();
     }
->>>>>>> branch 'master' of https://gitlab.univ-lille.fr/francois.deroubaix.etu/projetmode-i6
+
     public static void main(String[] args) {
-<<<<<<< HEAD
-    	
-        Application.launch();
-=======
         launch(args);
->>>>>>> branch 'master' of https://gitlab.univ-lille.fr/francois.deroubaix.etu/projetmode-i6
     }
 }
