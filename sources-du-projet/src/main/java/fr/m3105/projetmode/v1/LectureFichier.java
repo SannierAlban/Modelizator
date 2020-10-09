17e5c60413c287cc50031d9eb572dd9f459c92a7
Marti <Marti@DESKTOP-UDRD15C>
2020-10-09 11:58:38 +0200

    Parsing - vertex

--- sources-du-projet/src/main/java/fr/m3105/projetmode/v1/LectureFichier.java
+++ sources-du-projet/src/main/java/fr/m3105/projetmode/v1/LectureFichier.java
@@ -3,6 +3,8 @@ package fr.m3105.projetmode.v1;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LectureFichier {
 	
@@ -18,6 +20,7 @@ public class LectureFichier {
 			String [] format=new String[] {reader.readLine(), reader.readLine()};
 			
 			if (!verifFormat(format)) System.exit(1);
+			int vertex=this.lectureVertex(reader.readLine());
 			
 		}catch(IOException | ErreurFichierException e) {
 			e.printStackTrace();
@@ -26,13 +29,31 @@ public class LectureFichier {
 	
 	private boolean verifFormat(String[] l) throws ErreurFichierException {
 		if (l[0]==null || l[1]==null) throw new ErreurFichierException("Erreur: fichier vide");
-		if (!l[0].equals("ply")) throw new ErreurFichierException("Erreur: format incompatible (format n√©cessaire: ply) -ligne 1-");
-		if (!l[1].equals("format ascii 1.0")) throw new ErreurFichierException("Erreur: format incompatible (format n√©cessaire: ascii 1.0) -ligne 2-");
+		if (!l[0].equals("ply")) throw new ErreurFichierException("Erreur: format incompatible (format necessaire: ply) -ligne 1-");
+		if (!l[1].equals("format ascii 1.0")) throw new ErreurFichierException("Erreur: format incompatible (format necessaire: ascii 1.0) -ligne 2-");
 		return true;
 	}
 	
-	private int lectureVertex(String l) {
-		//if (!l.substring(0,14).equals("element vertex"))
-		return 0;
+	private int lectureVertex(String l) throws ErreurFichierException{
+		if (!l.substring(0,14).equals("element vertex")) throw new ErreurFichierException("Erreur : mauvaise dÈclaration des points -ligne 3-");
+		String vertex=l.substring(15);
+		int res=this.isInt(vertex);
+		if(res<=0) throw new ErreurFichierException("Erreur: le nombre de points doit Ítre >0");
+		return res;
+	}
+	
+	private int isInt(String testing) {
+		try {
+			int res=Integer.parseInt(testing);
+			return res;
+		}catch(Exception e) {
+			return -1;
+		}
+	}
+	
+	private List<String> lectureProperties() {
+		List<String> res=new ArrayList<String>();
+		
+		return null; 
 	}
 }
