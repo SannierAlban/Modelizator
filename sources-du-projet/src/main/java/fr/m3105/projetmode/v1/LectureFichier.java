4c3aec2a6d834a06c5df4bf36cfccba68047d794
Marti <Marti@DESKTOP-UDRD15C>
2020-10-09 16:50:50 +0200

    Parsing - faces

--- sources-du-projet/src/main/java/fr/m3105/projetmode/v1/LectureFichier.java
+++ sources-du-projet/src/main/java/fr/m3105/projetmode/v1/LectureFichier.java
@@ -1,6 +1,7 @@
 package fr.m3105.projetmode.v1;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,35 +10,52 @@ import java.util.List;
 public class LectureFichier {
 	
 	BufferedReader reader;
+	protected int vertex;
+	protected List<String> props;
+	protected int faces;
 	
-	public void lecture(String path) throws IOException{
+	LectureFichier(String path) throws IOException{
+		this.lecture(path);
+	}
+	
+	private void ouvertureFichier(String path) throws ErreurFichierException {
+		try {
+			reader= new BufferedReader(new FileReader(path));
+		} catch (FileNotFoundException e) {
+			throw new ErreurFichierException("Erreur: fichier introuvable");
+		}
+	}
+	
+	private void lecture(String path) throws IOException{
 		
 		
 		try {
-			BufferedReader reader= new BufferedReader(new FileReader(path));
-			
-			String line;
+	
+			this.ouvertureFichier(path);
 			String [] format=new String[] {reader.readLine(), reader.readLine()};
 			
 			if (!verifFormat(format)) System.exit(1);
-			int vertex=this.lectureVertex(reader.readLine());
+			vertex=this.lectureVertex(reader.readLine());
+			props=this.lectureProperties();
+			this.faces=this.lectureFace(reader.readLine());
+			
 			
 		}catch(IOException | ErreurFichierException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	private boolean verifFormat(String[] l) throws ErreurFichierException {
-		if (l[0]==null || l[1]==null) throw new ErreurFichierException("Erreur: fichier vide");
-		if (!l[0].equals("ply")) throw new ErreurFichierException("Erreur: format incompatible (format necessaire: ply) -ligne 1-");
-		if (!l[1].equals("format ascii 1.0")) throw new ErreurFichierException("Erreur: format incompatible (format necessaire: ascii 1.0) -ligne 2-");
+	private boolean verifFormat(String[] lines) throws ErreurFichierException {
+		if (lines[0]==null || lines[1]==null) throw new ErreurFichierException("Erreur: fichier vide");
+		if (!lines[0].equals("ply")) throw new ErreurFichierException("Erreur: format incompatible (format necessaire: ply) -ligne 1-");
+		if (!lines[1].equals("format ascii 1.0")) throw new ErreurFichierException("Erreur: format incompatible (format necessaire: ascii 1.0) -ligne 2-");
 		return true;
 	}
 	
-	private int lectureVertex(String l) throws ErreurFichierException{
-		if (!l.substring(0,14).equals("element vertex")) throw new ErreurFichierException("Erreur : mauvaise déclaration des points -ligne 3-");
-		String vertex=l.substring(15);
-		int res=this.isInt(vertex);
+	private int lectureVertex(String line) throws ErreurFichierException{
+		if (!line.substring(0,14).equals("element vertex")) throw new ErreurFichierException("Erreur : mauvaise déclaration des points -ligne 3-");
+		String vertexStr=line.substring(15);
+		int res=this.isInt(vertexStr);
 		if(res<=0) throw new ErreurFichierException("Erreur: le nombre de points doit être >0");
 		return res;
 	}
@@ -51,9 +69,21 @@ public class LectureFichier {
 		}
 	}
 	
-	private List<String> lectureProperties() {
+	private List<String> lectureProperties() throws IOException {
 		List<String> res=new ArrayList<String>();
-		
-		return null; 
+		String current;
+		while ((current=reader.readLine()).substring(0,8).equals("property")) {
+			res.add(current.substring(9));
+		}
+		return res; 
 	}
+	
+	private int lectureFace(String line) throws ErreurFichierException {
+		if (!line.substring(0,12).equals("element face")) throw new ErreurFichierException("Erreur : mauvaise déclaration des faces");
+		String faceStr=line.substring(15);
+		int res=this.isInt(faceStr);
+		if(res<=0) throw new ErreurFichierException("Erreur: le nombre de faces doit être >0");
+		return res;
+	}
+	
 }
