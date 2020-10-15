c7863c2bed90a27eac116ec78146aa2cf08e30c3
Martin Dorange <martin.dorange@protonmail.com>
2020-10-15 08:35:58 +0200

    gestion des commentaires

--- sources-du-projet/src/main/java/fr/m3105/projetmode/v1/LectureFichier.java
+++ sources-du-projet/src/main/java/fr/m3105/projetmode/v1/LectureFichier.java
@@ -32,17 +32,23 @@ public class LectureFichier {
 		try {
 	
 			this.ouvertureFichier(path);
-			String [] format=new String[] {reader.readLine(), reader.readLine()};
+			String [] format=new String[] {this.getLine(), this.getLine()};
 			
 			if (!verifFormat(format)) System.exit(1);
-			vertex=this.lectureVertex(reader.readLine());
-			props=this.lectureProperties();
-			this.faces=this.lectureFace(reader.readLine());
+			this.vertex=this.lectureVertex(this.getLine());
+			this.props=this.lectureProperties();
+			this.faces=this.lectureFace(this.getLine());
 			
 			
 		}catch(IOException | ErreurFichierException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private String getLine() throws IOException {
+		String current=reader.readLine();
+		while (this.isComment((current=reader.readLine())));
+		return current;
 	}
 	
 	private boolean verifFormat(String[] lines) throws ErreurFichierException {
@@ -53,10 +59,10 @@ public class LectureFichier {
 	}
 	
 	private int lectureVertex(String line) throws ErreurFichierException{
-		if (!line.substring(0,14).equals("element vertex")) throw new ErreurFichierException("Erreur : mauvaise déclaration des points -ligne 3-");
+		if (!line.substring(0,14).equals("element vertex")) throw new ErreurFichierException("Erreur : mauvaise declaration des points -ligne 3-");
 		String vertexStr=line.substring(15);
 		int res=this.isInt(vertexStr);
-		if(res<=0) throw new ErreurFichierException("Erreur: le nombre de points doit être >0");
+		if(res<=0) throw new ErreurFichierException("Erreur: le nombre de points doit etre >0");
 		return res;
 	}
 	
@@ -72,18 +78,22 @@ public class LectureFichier {
 	private List<String> lectureProperties() throws IOException {
 		List<String> res=new ArrayList<String>();
 		String current;
-		while ((current=reader.readLine()).substring(0,8).equals("property")) {
+		while ((current=this.getLine()).substring(0,8).equals("property")) {
 			res.add(current.substring(9));
 		}
 		return res; 
 	}
 	
 	private int lectureFace(String line) throws ErreurFichierException {
-		if (!line.substring(0,12).equals("element face")) throw new ErreurFichierException("Erreur : mauvaise déclaration des faces");
+		if (!line.substring(0,12).equals("element face")) throw new ErreurFichierException("Erreur : mauvaise dï¿½claration des faces");
 		String faceStr=line.substring(15);
 		int res=this.isInt(faceStr);
-		if(res<=0) throw new ErreurFichierException("Erreur: le nombre de faces doit être >0");
+		if(res<=0) throw new ErreurFichierException("Erreur: le nombre de faces doit ï¿½tre >0");
 		return res;
+	}
+	
+	private boolean isComment(String line) {
+		return (line.substring(0,7).toLowerCase().equals("comment")); 
 	}
 	
 }
