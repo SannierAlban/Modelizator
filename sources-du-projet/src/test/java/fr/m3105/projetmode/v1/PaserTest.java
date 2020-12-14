858f3082ed5946b791d0e7c5c818dc865a502d00
Alban Sannier <alban.sannier.etu@univ-lille.fr>
2020-12-14 13:27:19 +0100

    Parser and testParser update for change

--- sources-du-projet/src/test/java/fr/m3105/projetmode/v1/PaserTest.java
+++ sources-du-projet/src/test/java/fr/m3105/projetmode/v1/PaserTest.java
@@ -2,8 +2,7 @@ package fr.m3105.projetmode.v1;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
-
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,79 +12,151 @@ import fr.m3105.projetmode.model.Point;
 
 class PaserTest {
 
-	Parser parserTest;
+	static Parser parserTestDe34;
+	static Parser parserTestMeuh;
+	static Parser parserTestCrane;
 
+	@BeforeAll
+	static void init() {
+		parserTestDe34 = new Parser("exemples/de34.ply");
+		parserTestMeuh = new Parser("exemples/meuh.ply");
+		parserTestCrane = new Parser("exemples/crane.ply");
+	}
+	
+	@Test
+	void testHeaderForDe34() {
+		assertEquals(16,parserTestDe34.getNbFaces());
+		assertEquals(10,parserTestDe34.getVertex());
+		assertEquals(true, parserTestDe34.isColor());
+		assertEquals(false, parserTestDe34.isAlpha());
+	}
+	
 	@Test
 	void testHeaderForCrane() {
-		parserTest = new Parser("exemples/crane.ply");
-		assertEquals(248999,parserTest.nbFaces);
-		assertEquals(133009,parserTest.vertex);
-		assertEquals(true, parserTest.color);
-		assertEquals(false, parserTest.alpha);
-		
-		
+		assertEquals(248999,parserTestCrane.getNbFaces());
+		assertEquals(133009,parserTestCrane.getVertex());
+		assertEquals(true, parserTestCrane.isColor());
+		assertEquals(false, parserTestCrane.isAlpha());
 	}
 	
 	@Test
-	void TestHeaderForDragon() {
-		parserTest = new Parser("exemples/dragon.ply");
-		assertEquals(871414,parserTest.nbFaces);
-		assertEquals(437645,parserTest.vertex);
-		assertEquals(false, parserTest.color);
-		assertEquals(false, parserTest.alpha);
-	}
-
-	@Test
-	void TestReadPointsForCrane() {
-		parserTest = new Parser("exemples/crane.ply");
-		ArrayList<Point> points = parserTest.points;
-		Point pointTest;
-		
-		assertEquals(parserTest.vertex, parserTest.points.size());
-		
-		pointTest = new Point(-0.000000 ,-0.000000 ,-0.000000);
-		assertEquals(pointTest,	points.get(0));
-		pointTest = new Point(-18.061632 ,-119.617180 ,70.996452);
-		assertEquals(pointTest, points.get(points.size()-1));
+	void testHeaderForMeuh() {
+		assertEquals(5804,parserTestMeuh.getNbFaces());
+		assertEquals(2903,parserTestMeuh.getVertex());
+		assertEquals(false, parserTestMeuh.isColor());
+		assertEquals(false, parserTestMeuh.isAlpha());
 	}
 	
 	@Test
-	void TestReadFacesForCrane() {
-		parserTest = new Parser("exemples/crane.ply");
-		ArrayList<Point> points = parserTest.points;
-		ArrayList<Face> faces = parserTest.faces;
+	void testPointsForDe34() {
+		double[][] points = parserTestDe34.getPoints();
+		assertEquals(0.1, points[0][0]);
+		assertEquals(0.1, points[1][0]);
+		assertEquals(0.1, points[2][0]);
 		
-		ArrayList<Point> arrayPointsTest = new ArrayList<Point>();
-		
-		arrayPointsTest.add(points.get(2));			//premiere valeur
-		arrayPointsTest.add(points.get(1));
-		arrayPointsTest.add(points.get(0));
-		Face faceTest = new Face(arrayPointsTest);
-		assertEquals(faceTest, faces.get(0));
-		
-		arrayPointsTest.clear();					//deuxieme dernier valeur
-		arrayPointsTest.add(points.get(3));	
-		arrayPointsTest.add(points.get(2));
-		arrayPointsTest.add(points.get(0));
-		faceTest = new Face(arrayPointsTest);
-		
-		assertEquals(faceTest, faces.get(1));
-				
-		arrayPointsTest.clear();					//avant dernier valeur
-		arrayPointsTest.add(points.get(127597));	
-		arrayPointsTest.add(points.get(127599));
-		arrayPointsTest.add(points.get(127598));
-		faceTest = new Face(arrayPointsTest);
-		
-		assertEquals(faceTest, faces.get(faces.size()-2));
-		
-		arrayPointsTest.clear();					//dernier test
-		arrayPointsTest.add(points.get(122235));
-		arrayPointsTest.add(points.get(122230));
-		arrayPointsTest.add(points.get(122229));
-		faceTest = new Face(arrayPointsTest);
-		
-		assertEquals(faceTest, faces.get(faces.size()-1));
+		assertEquals(2.1, points[0][9]);
+		assertEquals(4.1, points[1][9]);
+		assertEquals(4.1, points[2][9]);
 	}
 	
+	@Test
+	void testPointsForCrane() {
+		double[][] points = parserTestCrane.getPoints();
+		assertEquals(-0.0, points[0][0]);
+		assertEquals(-0.0, points[1][0]);
+		assertEquals(-0.0, points[2][0]);
+		
+		int lastIdx = parserTestCrane.getVertex() - 1;
+		assertEquals(-18.061632, points[0][lastIdx]);
+		assertEquals(-119.617180, points[1][lastIdx]);
+		assertEquals(70.996452, points[2][lastIdx]);
+	}
+	
+	@Test
+	void testPointsForMeuh() {
+		double[][] points = parserTestMeuh.getPoints();
+		assertEquals(0.605538, points[0][0]);
+		assertEquals(0.183122, points[1][0]);
+		assertEquals(-0.472278, points[2][0]);
+		
+		int lastIdx = parserTestMeuh.getVertex() - 1;
+		assertEquals(-1.48926, points[0][lastIdx]);
+		assertEquals(0.64369, points[1][lastIdx]);
+		assertEquals(0.227226, points[2][lastIdx]);
+	}
+	
+	@Test
+	void testFacesForDe34() {
+		int [][] faces = parserTestDe34.getFaces();
+		assertEquals(0, faces[0][0]);
+		assertEquals(1, faces[1][0]);
+		assertEquals(3, faces[2][0]);
+		int lastIdx = parserTestDe34.getNbFaces()-1;
+		assertEquals(7, faces[0][lastIdx]);
+		assertEquals(9, faces[1][lastIdx]);
+		assertEquals(8, faces[2][lastIdx]);
+	}
+	
+	@Test
+	void testFacesForCrane() {
+		int [][] faces = parserTestCrane.getFaces();
+		assertEquals(3, faces[0][0]);
+		assertEquals(2, faces[1][0]);
+		assertEquals(1, faces[2][0]);
+		int lastIdx = parserTestCrane.getNbFaces()-1;
+		assertEquals(122236, faces[0][lastIdx]);
+		assertEquals(122231, faces[1][lastIdx]);
+		assertEquals(122230, faces[2][lastIdx]);
+	}
+	
+	@Test
+	void testFacesForMeuh() {
+		int [][] faces = parserTestMeuh.getFaces();
+		assertEquals(0, faces[0][0]);
+		assertEquals(1, faces[1][0]);
+		assertEquals(2, faces[2][0]);
+		int lastIdx = parserTestMeuh.getNbFaces()-1;
+		assertEquals(2498, faces[0][lastIdx]);
+		assertEquals(2381, faces[1][lastIdx]);
+		assertEquals(2433, faces[2][lastIdx]);
+	}
+	
+	@Test
+	void testRGBforDe34() {
+		int [][] rgbAlpha = parserTestDe34.getRgbAlpha();
+		int idxMaxFace = parserTestDe34.getNbFaces();
+		assertEquals(3, rgbAlpha.length);
+		assertEquals(idxMaxFace, rgbAlpha[0].length);
+		
+		assertEquals(255, rgbAlpha[0][0]);
+		assertEquals(128, rgbAlpha[1][0]);
+		assertEquals(128, rgbAlpha[2][0]);
+		
+		assertEquals(128, rgbAlpha[0][idxMaxFace - 1]);
+		assertEquals(128, rgbAlpha[1][idxMaxFace - 1]);
+		assertEquals(128, rgbAlpha[2][idxMaxFace - 1]);
+	}
+	
+	@Test
+	void testRGBforCrane() {
+		int [][] rgbAlpha = parserTestCrane.getRgbAlpha();
+		assertEquals(3, rgbAlpha.length);
+		int idxMaxVertex = parserTestCrane.getVertex();
+		assertEquals( idxMaxVertex , rgbAlpha[0].length);
+		
+		assertEquals(41, rgbAlpha[0][0]);
+		assertEquals(35, rgbAlpha[1][0]);
+		assertEquals(21, rgbAlpha[2][0]);
+		
+		assertEquals(100, rgbAlpha[0][idxMaxVertex - 1]);
+		assertEquals(73, rgbAlpha[1][idxMaxVertex - 1]);
+		assertEquals(62, rgbAlpha[2][idxMaxVertex - 1]);
+	}
+	
+	@Test
+	void testRGBforMeuh() {
+		assertNull(parserTestMeuh.getRgbAlpha());
+		assertFalse(parserTestMeuh.isColor());
+		assertFalse(parserTestMeuh.isAlpha());
+	}
 }
