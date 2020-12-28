b16cc766f69e2b2af2a0aeee39d582b024c94106
Lohan Calot <lohan.calot.etu@univ-lille.fr>
2020-12-28 13:20:09 +0100

    added getNormalVector method

--- sources-du-projet/src/main/java/fr/m3105/projetmode/model/Model.java
+++ sources-du-projet/src/main/java/fr/m3105/projetmode/model/Model.java
@@ -231,11 +231,18 @@ public class Model {
 			double[] normalVector = getNormalVector(idxFace);
 		}
 	}
-
+	/**
+	 * Returns the normal vector using the coordinates of a Face's points. <br>
+	 * Basically this method calculates the cross product of the two first points located at the idxFace index.
+	 * @param idxFace the index of the face you wish to get the normal vector
+	 * @return double[3] the normal vector
+	 */
 	private double[] getNormalVector(int idxFace) {
-		double[] res = new double[3];
-		return res;
-		
+		double[] vector1 = getPoint(FACES[0][idxFace]);
+		double[] vector2 = getPoint(FACES[1][idxFace]);
+		return new double[] {vector1[1]*vector2[2] - vector1[2]*vector2[1],
+				vector1[2]*vector2[0] - vector1[0]*vector2[2],
+				vector1[0]*vector2[1] - vector1[1]*vector2[0]};
 	}
 
 	/**
